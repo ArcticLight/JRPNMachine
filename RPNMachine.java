e7c74e8436d6a7741be35fd3450172f19f4ef79d
Hawk Weisman <hawk.weisman@gmail.com>
2013-03-29 15:46:36 -0300

    Update RPNMachine.java
    subtract should work now :)

--- RPNMachine.java
+++ RPNMachine.java
@@ -4,6 +4,9 @@ public class RPNMachine {
 		Operation op;
 		enum Operation { DATA, ADD, SUBTRACT }
 		
+		/**
+		 * Evaluates whether a StackMember is an operation or operand.
+		 */
 		public void notData () {
 			if(this.op != DATA) throw new ArithmeticException("Stack item " + this + " is not an integer.");	
 		}
@@ -25,7 +28,13 @@ public class RPNMachine {
 					break;
 				case SUBTRACT:
 					StackMember next = stack.pop();
-					if(next.op != DATA) throw new ArithmeticException("Stack item " + next + " is not an integer.");
+					next.notData();
+					double a = next.datavalue;
+					next = stack.pop();
+					next.notData();
+					double b = next.datavalue();
+					stack.pop (a - b);
+					break;
 			}
 		}
 	}
