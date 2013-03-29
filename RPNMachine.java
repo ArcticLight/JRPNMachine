b73d6ec9fc4888d44f302d936858370d38a8e712
Hawk Weisman <hawk.weisman@gmail.com>
2013-03-29 13:16:56 -0300

    Update RPNMachine.java

--- RPNMachine.java
+++ RPNMachine.java
@@ -3,6 +3,10 @@ public class RPNMachine {
 		double datavalue;
 		Operation op;
 		enum Operation { DATA, ADD, SUBTRACT }
+		
+		public void notData () {
+			if(this.op != DATA) throw new ArithmeticException("Stack item " + this + " is not an integer.");	
+		}
 
 		public void eval(java.util.Stack<StackMember> stack) {
 			StackMember current = stack.pop();
@@ -12,8 +16,16 @@ public class RPNMachine {
 					break;
 				case ADD:
 					StackMember next = stack.pop();
-					if(next.op != DATA) throw new ArithmeticException("Stack item " + next + " is not an integer.");
+					next.notData();
 					double a = next.datavalue;
+					next = stack.pop();
+					next.notData();
+					double b = next.datavalue;
+					stack.pop (a + b);
+					break;
+				case SUBTRACT:
+					StackMember next = stack.pop();
+					if(next.op != DATA) throw new ArithmeticException("Stack item " + next + " is not an integer.");
 			}
 		}
 	}
