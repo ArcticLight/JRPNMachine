5173e066ecfb892cb61a4ee3ed75b43c501798e6
Hawk Weisman <hawk.weisman@gmail.com>
2013-03-29 16:38:53 -0400

    fixed eval
    now gets values from getDataValue()

--- com/meteorcode/JRPNMachine/RPNMachine.java
+++ com/meteorcode/JRPNMachine/RPNMachine.java
@@ -6,43 +6,43 @@ public class RPNMachine {
 	 * Evaluates the top member of the stack given as a parameter
 	 * @param the stack to be evaluated
 	 */
-	public void eval(java.util.Stack<StackMember> stack) {
+	public void eval (java.util.Stack<StackMember> stack) {
 		StackMember current = stack.pop();
 		switch(current.op) {
 			case DATA:
-			stack.push(current);
-			break;
+				stack.push(current);
+				break;
 			case ADD:
 				StackMember next = stack.pop();
 				next.notData();
-				double a = next.datavalue;
+				double a = next.getValue();
 				next = stack.pop();
 				next.notData();
-				double b = next.datavalue;
+				double b = next.getValue();
 				stack.push (new StackMember (a + b));
 				break;
 			case SUBTRACT:
 				StackMember next = stack.pop();
 				next.notData();
-				double a = next.datavalue;
+				double a = next.getValue();
 				next = stack.pop();
 				next.notData();
-				double b = next.datavalue();
+				double b = next.getValue();
 				stack.push (new StackMember (a - b));
 				break;
 			case MULTIPLY:
 				StackMember next = stack.pop();
 				next.notData();
-				double a = next.datavalue;
+				double a = next.getValue();
 				next = stack.pop();
 				next.notData();
-				double b = next.datavalue();
+				double b = next.getValue();
 				stack.push (new StackMember (a * b));
 				break;
 			case DIVIDE:
 				StackMember next = stack.pop();
 				next.notData();
-				double a = next.datavalue;
+				double a = next.getValue();
 				next = stack.pop();
 				next.notData();
 				double b = next.datavalue();
@@ -51,7 +51,7 @@ public class RPNMachine {
 			case MODULO:
 				StackMember next = stack.pop();
 				next.notData();
-				double a = next.datavalue;
+				double a = next.getValue();
 				next = stack.pop();
 				next.notData();
 				double b = next.datavalue();
