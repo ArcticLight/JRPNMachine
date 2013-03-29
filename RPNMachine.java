561372123efc31f476e01b93e15ad2da36e96810
Hawk Weisman <hawk.weisman@gmail.com>
2013-03-29 17:23:35 -0300

    Update RPNMachine.java

--- RPNMachine.java
+++ RPNMachine.java
@@ -19,63 +19,63 @@ public class RPNMachine {
 		public void notData () {
 			if(this.op != DATA) throw new ArithmeticException("Stack item " + this + " is not an integer.");	
 		}
+	}
 		
-		/**
-		 * Evaluates the top member of the stack given as a parameter
-		 * @param the stack to be evaluated
-		 */
-		public void eval(java.util.Stack<StackMember> stack) {
-			StackMember current = stack.pop();
-			switch(current.op) {
-				case DATA:
-					stack.push(current);
-					break;
-				case ADD:
-					StackMember next = stack.pop();
-					next.notData();
-					double a = next.datavalue;
-					next = stack.pop();
-					next.notData();
-					double b = next.datavalue;
-					stack.pop (a + b);
-					break;
-				case SUBTRACT:
-					StackMember next = stack.pop();
-					next.notData();
-					double a = next.datavalue;
-					next = stack.pop();
-					next.notData();
-					double b = next.datavalue();
-					stack.pop (a - b);
-					break;
-				case MULTIPLY:
-					StackMember next = stack.pop();
-					next.notData();
-					double a = next.datavalue;
-					next = stack.pop();
-					next.notData();
-					double b = next.datavalue();
-					stack.pop (a * b);
-					break;
-				case DIVIDE:
-					StackMember next = stack.pop();
-					next.notData();
-					double a = next.datavalue;
-					next = stack.pop();
-					next.notData();
-					double b = next.datavalue();
-					stack.pop (a / b);
-					break;
-				case MODULO:
-					StackMember next = stack.pop();
-					next.notData();
-					double a = next.datavalue;
-					next = stack.pop();
-					next.notData();
-					double b = next.datavalue();
-					stack.pop (a % b);
-					break;
-			}
+	/**
+	 * Evaluates the top member of the stack given as a parameter
+	 * @param the stack to be evaluated
+	 */
+	public void eval(java.util.Stack<StackMember> stack) {
+		StackMember current = stack.pop();
+		switch(current.op) {
+			case DATA:
+			stack.push(current);
+			break;
+			case ADD:
+				StackMember next = stack.pop();
+				next.notData();
+				double a = next.datavalue;
+				next = stack.pop();
+				next.notData();
+				double b = next.datavalue;
+				stack.pop (a + b);
+				break;
+			case SUBTRACT:
+				StackMember next = stack.pop();
+				next.notData();
+				double a = next.datavalue;
+				next = stack.pop();
+				next.notData();
+				double b = next.datavalue();
+				stack.pop (a - b);
+				break;
+			case MULTIPLY:
+				StackMember next = stack.pop();
+				next.notData();
+				double a = next.datavalue;
+				next = stack.pop();
+				next.notData();
+				double b = next.datavalue();
+				stack.pop (a * b);
+				break;
+			case DIVIDE:
+				StackMember next = stack.pop();
+				next.notData();
+				double a = next.datavalue;
+				next = stack.pop();
+				next.notData();
+				double b = next.datavalue();
+				stack.pop (a / b);
+				break;
+			case MODULO:
+				StackMember next = stack.pop();
+				next.notData();
+				double a = next.datavalue;
+				next = stack.pop();
+				next.notData();
+				double b = next.datavalue();
+				stack.pop (a % b);
+				break;
 		}
 	}
 }
