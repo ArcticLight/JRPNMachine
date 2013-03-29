dff4e92f9c686725cf3c66f1e244cb153f306f6d
Max <mattrulz127@gmail.com>
2013-03-29 17:27:44 -0300

    Remove StackMember
    Prepare to separate into a different file; also, package declaration.

--- com/meteorcode/JRPNMachine/RPNMachine.java
+++ com/meteorcode/JRPNMachine/RPNMachine.java
@@ -1,25 +1,6 @@
+package com.meteorcode.JRPNMachine;
+
 public class RPNMachine {
-	
-	/**
-	 * A member (either an operator or operand) of the RPN machine's execution stack.
-	 * @author Hawk Weisman
-	 * @author Max Clive
-	 * 
-	 */
-	public static class StackMember {
-		double datavalue;							// the numerical value of this stack member
-		Operation op;								// the operation type of this stack member
-		enum Operation { DATA, ADD, SUBTRACT, MULTIPLY, DIVIDE, MODULO }	// possible operation identities for stack members
-		
-		
-		/**
-		 * Throws an ArithmeticException if the calling StackMember is not an operand
-		 * @throws ArithmeticException
-		 */
-		public void notData () {
-			if(this.op != DATA) throw new ArithmeticException("Stack item " + this + " is not an integer.");	
-		}
-	}
 		
 	/**
 	 * Evaluates the top member of the stack given as a parameter
