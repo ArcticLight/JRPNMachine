589da447718d5b9e02279b9c2e2255eed11d29a9
Hawk Weisman <hawk.weisman@gmail.com>
2013-03-29 15:02:18 -0400

    more ops!
    added multiply, divide, and modulo Operations (I assume we want those)

--- RPNMachine.java
+++ RPNMachine.java
@@ -1,11 +1,19 @@
 public class RPNMachine {
+	
+	/**
+	 * A member (either an operator or operand) of the RPN machine's execution stack.
+	 * @author Hawk Weisman
+	 * @author Max Clive
+	 * 
+	 */
 	public static class StackMember {
-		double datavalue;
-		Operation op;
-		enum Operation { DATA, ADD, SUBTRACT }
+		double datavalue;							// the numerical value of this stack member
+		Operation op;								// the operation type of this stack member
+		enum Operation { DATA, ADD, SUBTRACT, MULTIPLY, DIVIDE, MODULO }	// possible operation identities for stack members
 		
 		/**
 		 * Throws an ArithmeticException if the calling StackMember is not an operand
+		 * @throws ArithmeticException
 		 */
 		public void notData () {
 			if(this.op != DATA) throw new ArithmeticException("Stack item " + this + " is not an integer.");	
@@ -38,6 +46,33 @@ public class RPNMachine {
 					next.notData();
 					double b = next.datavalue();
 					stack.pop (a - b);
+					break;
+				case MULTIPLY:
+					StackMember next = stack.pop();
+					next.notData();
+					double a = next.datavalue;
+					next = stack.pop();
+					next.notData();
+					double b = next.datavalue();
+					stack.pop (a * b);
+					break;
+				case DIVIDE:
+					StackMember next = stack.pop();
+					next.notData();
+					double a = next.datavalue;
+					next = stack.pop();
+					next.notData();
+					double b = next.datavalue();
+					stack.pop (a / b);
+					break;
+				case MODULO:
+					StackMember next = stack.pop();
+					next.notData();
+					double a = next.datavalue;
+					next = stack.pop();
+					next.notData();
+					double b = next.datavalue();
+					stack.pop (a % b);
 					break;
 			}
 		}
