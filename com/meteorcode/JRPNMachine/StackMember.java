6f29316d2d97616cf260b6019334a641571d9939
Hawk Weisman <hawk.weisman@gmail.com>
2013-04-02 15:18:28 -0400

    Added toString() to StackMember.

--- com/meteorcode/JRPNMachine/StackMember.java
+++ com/meteorcode/JRPNMachine/StackMember.java
@@ -8,33 +8,72 @@
 package com.meteorcode.JRPNMachine;
 
 public class StackMember {
-    double datavalue;							// the numerical value of this stack member
+    double value;							// the numerical value of this stack member
     Operation op;							// the operation type of this stack member
     enum Operation { DATA, ADD, SUBTRACT, MULTIPLY, DIVIDE, MODULO, DELETE }	// possible operation identities for stack members
 	
-	
+	/**
+	 * constructor: creates a new StackMember with a value (operand)
+	 * @param value
+	 */
 	public StackMember (double value) {
-		this.datavalue = value;
+		this.value = value;
 		this.op = Operation.DATA;
 	}
 	
+	/**
+	 * constructor: creates a new StackMember with an operation (operator)
+	 * @param op 
+	 */
 	public StackMember (Operation op) {
 		this.op = op;
 	}
 	
+	/**
+	 * accessor for datavalue
+	 * @return the value of this StackMember (0 if it is an operator)
+	 */
 	public double getValue () {
-		return this.datavalue;
+		return this.value;
 	}
 	
+	/**
+	 * accessor for op
+	 * @return the operation identity of this StackMember
+	 */
 	public Operation getOp () {
 		return this.op;
 	}
 
 	/**
-	 * Throws an ArithmeticException if the calling StackMember is not an operand
+	 * test to see if the calling StackMember is not an operand
 	 * @throws ArithmeticException
 	 */
 	public void notData () {
 		if(this.op != Operation.DATA) throw new ArithmeticException("Stack item " + this + " is not an integer.");	
 	}
+	
+	/**
+	 * toString
+	 * @return a representation of this StackMember as a string
+	 */
+	 public String toString () {
+		switch (this.op) {
+			case DATA:
+				return "" + value;
+			case ADD:
+				return "+";
+			case SUBTRACT:
+				return "-";
+			case MULTIPLY:
+				return "*";
+			case DIVIDE:
+				return "/";
+			case MODULO:
+				return "%";
+			case DELETE:
+				return "delete";
+		}
+		return null;
+	 }
 }
