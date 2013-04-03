d748deb5aa013fa421df9b8b9a16aa34c567c95f
Max <mattrulz127@gmail.com>
2013-04-03 19:55:52 -0300

    Begin work on Polonium extra commands
    Commands stubbed such as precision and help.
    Also alias "exit" for "x"

--- Polonium.java
+++ Polonium.java
@@ -7,6 +7,9 @@ public class Polonium {
 	
 	public static final String P_VERSION = "0.3b";
 	
+	//declare Polonium specific instructions here please.
+	public static final String[] P_INSTRUCTIONS = {"x", "exit", "pd", "p", "pt", "h", "help"};
+	
 	public static void main(String[] args) {
 		java.util.Scanner in = new java.util.Scanner(System.in);
 		
@@ -21,11 +24,13 @@ public class Polonium {
 		String input = null;
 		
 		System.out.printf("[ ok ]\n\n");
+		List<String> instructions = java.util.Arrays.asList(P_INSTRUCTIONS);
 		do {
 			try {
 				System.out.printf("%s\n > ", machine.showState());
 				input = in.nextLine();
-				if(!input.equals("x"))
+				//if this is not a polonium instruction, eval it using the machine
+				if(!instructions.contains(input))
 				{
 					String[] temp = input.split(" ");
 					System.out.printf(">> %s : \"%s\"\n", machine.showState(), temp[0]);
@@ -33,6 +38,27 @@ public class Polonium {
 						machine.eval(temp[i]);
 						if(i != temp.length - 1)
 							System.out.printf(">> %s : \"%s\"\n", machine.showState(), temp[i+1]);
+					}
+				//otherwise, Polonium handles this input.
+				} else {
+					switch(input) {
+						//Do nothing on x; x is exit.
+						case "x":
+						case "exit":
+							break;
+						//pd, also p:  display precision mode
+						case "pd":
+						case "p":
+							//TODO: Implement this
+							break;
+						case "pt":
+							//TODO: Implement this
+							break;
+						//h and help: display help
+						case "h":
+						case "help":
+							//TODO: Implement help dialog/message
+							break;
 					}
 				}
 			} 
@@ -42,7 +68,7 @@ public class Polonium {
 			catch (Exception e) {
 				System.out.printf("\nERR: Type: %s\n Message: %s\n\n", e.getClass().getName(), e.getMessage());
 			}
-		} while(input != null && !input.equals("x"));
+		} while(input != null && !input.equals("x") && !input.equals("exit"));
 		
 		System.out.println("Goodbye.");
 		in.close();
